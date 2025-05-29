6e7f610780e63fe18626a2a691149e64d8f6ad3e
lc <284264018@qq.com>
2025-05-29 14:47:22 +0800

    feat: mdx code highlight https://chris.lu/web_development/tutorials/next-js-static-first-mdx-starterkit/code-highlighting-plugin#using-a-vscode-theme-from-a-git-repository

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,32 @@
-import createMDX from '@next/mdx';
-
-import type { NextConfig } from 'next';
+import createMDX from '@next/mdx'
+import type { NextConfig } from 'next'
+import rehypePrettyCode, { Options } from 'rehype-pretty-code'
+import rehypeMDXImportMedia from 'rehype-mdx-import-media'
 
 const nextConfig: NextConfig = {
     experimental: {
-        mdxRs: true
+        // mdxRs: true,
     },
     // Configure `pageExtensions` to include markdown and MDX files
-    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     // Optionally, add any other Next.js config below
-};
+}
 
+const rehypePrettyCodeOptions: Options = {
+    theme: 'one-dark-pro',
+}
 const withMDX = createMDX({
     // Add markdown plugins here, as desired
-});
+
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [
+            [rehypePrettyCode, rehypePrettyCodeOptions],
+            rehypeMDXImportMedia,
+        ],
+    },
+})
 
 // Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
